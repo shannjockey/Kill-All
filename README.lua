d22b723eb832d16975603aca24fe340733516174
._shannjockey <shanbarmmer@gmail.com>
2026-04-17 12:40:58 -0400

    Create README.lua for kill all script usage
    Add README.lua with instructions for a kill all script

---
+++ README.lua
@@ -0,0 +1,3 @@
+-- hello i made this kill all script for you to use in your game, just ennable https requests and run 
+```loadstring(game:HttpGet("https://raw.githubusercontent.com/shannjockey/Kill-All/refs/heads/main/Kill%20All.lua"))()```
+--hope it wronks in your game :D
